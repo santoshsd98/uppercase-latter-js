c1c1d1efe7116521bfb3cd7c99c190895ba5ff59
santoshsd98 <dikolesd98@gmail.com>
2023-01-17 16:31:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let fristName = "santosh";
+let lastName = "dikole";
+let state = "maharashatra";
+let country = "india";
+
+
+ console.log(fristName.toUpperCase());
+ console.log(lastName.toUpperCase());
+ console.log(state.toUpperCase());
+ console.log(country.toUpperCase());
+ 
